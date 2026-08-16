6ebd5a3ad5d1f8b99bbae71863094a19a34b335e
Zeeshan DevOps Engineer <zeeshan.devopsengineer@gmail.com>
2026-08-16 06:43:59 +0500

    Fix failing test

--- test_app.py
+++ test_app.py
@@ -2,8 +2,8 @@ from app import add, greet
 
 
 def test_add():
-#    assert add(2, 3) == 5
-assert add(2, 3) == 999
+    assert add(2, 3) == 5
+
 
 def test_greet():
     assert greet("Zeeshan") == "Hello, Zeeshan!"
